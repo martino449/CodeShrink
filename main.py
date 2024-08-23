66c612ac538f372a22b5b65f49f18836e8273ba3
Martin449 <76888581+martino449@users.noreply.github.com>
2024-08-23 19:27:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -124,3 +124,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+#Fine del codice
+# ---------------------------------------------
+# Copyright (c) 2024 Mario Pisano
+#
+# Questo programma è distribuito sotto la licenza EUPL, Versione 1.2 o – non appena 
+# saranno approvate dalla Commissione Europea – versioni successive della EUPL 
+# (la "Licenza");
+# Puoi usare, modificare e/o ridistribuire il programma sotto i termini della 
+# Licenza. 
+# 
+# Puoi trovare una copia della Licenza all'indirizzo:
+# https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
