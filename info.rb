7ce4f6be6dc1a545f20fe17eab965ebf2df0c68d
Martin449 <76888581+martino449@users.noreply.github.com>
2024-09-10 23:18:07 +0200

    Add files via upload

---
+++ info.rb
@@ -0,0 +1,23 @@
+# programma_info.rb
+#
+# Autore: Mario Pisano
+# Licenza: EUPL (European Union Public License)
+#
+
+
+def mostra_info()
+  nome_programma = 'CodeShrink'
+  versione = '0.0.3'
+  descrizione = 'CodeShrink 🐍✨ is a powerful and intuitive tool for compressing and decompressing Python scripts. With CodeShrink, you can easily reduce the size of your Python files while keeping the structure and functionality of your code intact.'
+
+  puts "Nome del programma: #{nome_programma}"
+  puts "Versione: #{versione}"
+  puts "Descrizione: #{descrizione}"
+end
+
+# Esecuzione del programma
+mostra_info()
+
+# Aspetta l'input dell'utente prima di terminare
+puts "Premi Invio per uscire..."
+gets
